18e1cac1a4dbfc0b784ae45d727bfdc2af157318
ivan.sabolic <ivan.sabolic@biss.hr>
2021-02-12 23:02:44 +0100

    setup global pipe (validation) and global filter (error handling)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { AllExceptionsFilter } from '@/app/util/all.exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get<string>('PORT'));
 }

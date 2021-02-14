d857a66d7032c92327f27d7b2a9112e955dbb51f
ivan.sabolic <ivan.sabolic@biss.hr>
2021-02-14 23:25:13 +0100

    TransformInterceptor

---
+++ src/app/interceptor/response.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+interface Response<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          successful: !data.message,
+          message: data.message,
+          data,
+        };
+      }),
+    ) as any;
+  }
+}
+
+export { TransformInterceptor };

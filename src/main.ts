0799ed2f68d915cfa6ca68281dcf0ebacdc9d999
ivan.sabolic <ivan.sabolic@biss.hr>
2021-02-14 23:19:13 +0100

    set global prefix, enable cors, include TransformInterceptor

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { AllExceptionsFilter } from '@/app/util/all.exceptions.filter';
+import { TransformInterceptor } from '@/app/interceptor/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get<string>('PORT'));
 }
